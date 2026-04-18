ad824a886f06b113e40155e84479209bb35d5e8d
Aleyna Bozoğlu <bozogluu61@gmail.com>
2026-04-18 22:47:21 +0300

    ilk_yukleme

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("Mini Katalog")),
+      body: Center(
+        child: Text("ÇALIŞIYOR 🚀"),
+      ),
+    ),
+  ));
+}
